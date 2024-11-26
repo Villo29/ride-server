7e86de1e5dc21aa86a58ab85be3f3c7f3d963f25
David Ruiz <jesuruga@gmail.com>
2024-11-25 21:03:50 -0600

    Eliminar valor predeterminado de host en la configuración del cliente de la base de datos

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const client = new Client({
     user: process.env.PG_USER || '',
-    host: process.env.PG_HOST || 'localhost',
+    host: process.env.PG_HOST || '',
     database: process.env.PG_DB || '',
     password: process.env.PG_PASSWORD || '',
     port: Number(process.env.PG_PORT) || 5432,
