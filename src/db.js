dfbac2e6556c9e42633ccc7b896ac8d8d38c4dfb
David Ruiz <jesuruga@gmail.com>
2024-11-25 20:47:43 -0600

    Eliminar valor predeterminado de host en la configuración de la base de datos y agregar mensaje de éxito en la conexión

--- src/db.js
+++ src/db.js
@@ -2,10 +2,12 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.PG_USER || "",
-  host: process.env.PG_HOST || "localhost",
+  host: process.env.PG_HOST || "",
   database: process.env.PG_DB || "",
   password: process.env.PG_PASSWORD || "",
   port: Number(process.env.PG_PORT) || 5432,
 });
+
+console.log("Conexión a la base de datos exitosa");
 
 module.exports = pool;
