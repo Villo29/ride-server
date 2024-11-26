5f0efb6e1296996daf2d106ead24fe084d709da0
David Ruiz <jesuruga@gmail.com>
2024-11-25 21:06:04 -0600

    Refactor conexión a la base de datos: reemplazar Client por Pool y agregar verificación de conexión

--- src/db.js
+++ src/db.js
@@ -1,25 +1,28 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const client = new Client({
-    user: process.env.PG_USER || '',
-    host: process.env.PG_HOST || '',
-    database: process.env.PG_DB || '',
-    password: process.env.PG_PASSWORD || '',
-    port: Number(process.env.PG_PORT) || 5432,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+// Configura el Pool
+const pool = new Pool({
+  user: process.env.PG_USER || '',
+  host: process.env.PG_HOST || '',
+  database: process.env.PG_DB || '',
+  password: process.env.PG_PASSWORD || '',
+  port: Number(process.env.PG_PORT) || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-client.connect()
-    .then(() => {
-        console.log('Conectado a la base de datos PostgreSQL exitosamente.');
-    })
-    .catch((error) => {
-        console.error('Error al conectar a la base de datos PostgreSQL:', error);
-    });
+// Verifica la conexión
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+  } else {
+    console.log('Conexión exitosa a la base de datos PostgreSQL.');
+    release(); // Libera el cliente después de usarlo
+  }
+});
 
-export default client;
+export default pool;
