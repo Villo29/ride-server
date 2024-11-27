e287f39be33a833f1ed3cfbe9c134000798178b0
David Ruiz <jesuruga@gmail.com>
2024-11-27 07:14:27 -0600

    Agregar carga de variables de entorno con dotenv en index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const dotenv = require("dotenv");
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const { Pool } = require("pg");
 const { v4: uuidv4 } = require("uuid");
-const { connect } = require("http2");
+require("dotenv").config(); 
 
 const app = express();
 const server = http.createServer(app);
