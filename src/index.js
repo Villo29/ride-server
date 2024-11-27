fa261a85a60a953cc1ec54fc6b29a3adb9474f87
David Ruiz <jesuruga@gmail.com>
2024-11-27 06:52:31 -0600

    hook agarrando de manera de ambos lados

--- src/index.js
+++ src/index.js
@@ -10,52 +10,43 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
-const pendingRides = new Map(); // Almacena viajes pendientes
+const pendingRides = new Map();
 
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  // Listener para recibir solicitudes de viaje
   socket.on("requestRide", (data) => {
     console.log("Ride requested:", data);
 
-    // Generar un rideId único
     const rideId = uuidv4();
 
-    // Guardar el viaje con el rideId
     const rideData = {
       ...data,
-      rideId, // Añadir el rideId generado
+      rideId,
     };
     pendingRides.set(rideId, rideData);
 
-    // Emitir la solicitud de viaje a todos los conductores conectados
     io.emit("newRideRequest", rideData);
 
     console.log("Nuevo viaje creado:", rideData);
   });
 
-  // Listener para recibir aceptación del conductor
   socket.on("acceptRide", (data) => {
     console.log("Ride accepted:", data);
 
-    // Verificar si el rideId existe en los viajes pendientes
     const ride = pendingRides.get(data.rideId);
 
     if (ride) {
       const passengerId = ride.passengerId;
 
-      // Combinar datos del viaje con datos del conductor
       const acceptedData = {
         ...ride,
         driverInfo: data.driverInfo,
         driverLocation: data.driverLocation,
       };
 
-      // Emitir al pasajero correspondiente
       io.to(passengerId).emit("rideAccepted", acceptedData);
 
-      // Eliminar el viaje de los pendientes
       pendingRides.delete(data.rideId);
 
       console.log("RideAccepted enviado al pasajero:", passengerId);
@@ -74,7 +65,6 @@ io.on("connection", (socket) => {
 
     console.log("Emitiendo tripEnded a passengerId:", data.passengerId);
 
-    // Emitir al pasajero correspondiente
     io.to(data.passengerId).emit("tripEnded", data);
 
     console.log("TripEnded enviado al pasajero:", data.passengerId);
